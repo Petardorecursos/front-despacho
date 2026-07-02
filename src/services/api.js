dbfb34942b8df83d812a7bfcb326537d4a4b4741
Petardorecursos <167472803+Petardorecursos@users.noreply.github.com>
2026-07-02 03:36:23 -0400

    Update API configuration with new Load Balancer URLs

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,15 @@
-// src/services/api.js
 import axios from 'axios';
 
-// Aquí pones la URL pública de tu Load Balancer de AWS
-// Si mañana cambias de servidor, solo cambias esta línea.
-const API_URL = "http://a16880a62dc0b4df6839bc9d52bcefa2-1297125742.us-east-1.elb.amazonaws.com";
+// URLs actualizadas con tus nuevos Load Balancers de AWS
+const URL_VENTAS = "http://a3f5e82467a7f44489a04b488bee868f-257877838.us-east-1.elb.amazonaws.com:8080";
+const URL_DESPACHOS = "http://a38705f70e11b4be294e4b8e9252b90d-1385445269.us-east-1.elb.amazonaws.com:8080";
 
-const api = axios.create({
-  baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+export const apiVentas = axios.create({
+  baseURL: URL_VENTAS,
+  headers: { 'Content-Type': 'application/json' },
 });
 
-export default api;
+export const apiDespacho = axios.create({
+  baseURL: URL_DESPACHOS,
+  headers: { 'Content-Type': 'application/json' },
+});
