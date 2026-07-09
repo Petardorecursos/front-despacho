449a80de7f32d6055a5c1e3216cccb3b3c6e5d9a
Petardorecursos <167472803+Petardorecursos@users.noreply.github.com>
2026-07-09 15:26:37 -0400

    Change button color scheme in Modal component

--- src/componentes/CrudAdmin/Modal.jsx
+++ src/componentes/CrudAdmin/Modal.jsx
@@ -18,7 +18,7 @@ export const Modal = ({ open, onClose, children }) => {
         <button
           type="button"
           onClick={onClose}
-          className="z-20 -mb-6 fill-emerald-500 hover:fill-emerald-600  font-bold hover:text-4xl text-3xl bg-teal-600 text-white transition-all w-14 h-14 "
+          className="z-20 -mb-6 fill-violet-500 hover:fill-violet-600  font-bold hover:text-4xl text-3xl bg-violet-600 text-white transition-all w-14 h-14 "
         >
           X
         </button>
